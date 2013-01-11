db142970973ed60239bdfe74274da68fe2868335
Alex Bond <alex.bondarevskiy@gmail.com>
2013-01-11 02:39:59 +0200

    Translate "Don't have perms"

--- src/main/java/com/sk89q/worldedit/WorldEdit.java
+++ src/main/java/com/sk89q/worldedit/WorldEdit.java
@@ -1392,7 +1392,7 @@ public class WorldEdit {
             try {
                 commands.execute(split, player, session, player, editSession);
             } catch (CommandPermissionsException e) {
-                player.printError("You don't have permission to do this.");
+                player.printError("У Вас не достаточноп рав на использование данной команды.");
             } catch (MissingNestedCommandException e) {
                 player.printError(e.getUsage());
             } catch (CommandUsageException e) {
